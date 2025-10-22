0a26a77f7ac474eeeddbfd12846c47787dbcffb7
Igor <igorreisbarbosa@gmail.com>
2025-10-21 22:03:24 -0300

    api problem

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from "next/server";
+import fs from 'fs/promises';
+import path from 'path';
 
-import DataHeroes from "../../../../public/database/heroes"
+// O caminho de disco para a pasta public/database/heroes.json
+const dataFilePath = path.join(process.cwd(), 'public', 'database', 'heroes.json');
 
-export async function GET() {    
-    // const res = await fetch(`${process.env.API_URL}`);
-    // const data = await res.json();
-    return NextResponse.json({ data: DataHeroes });
+export async function GET() {
+    try {
+        // 1. Lê o arquivo
+        const fileContents = await fs.readFile(dataFilePath, 'utf8');
+        
+        // 2. Converte o JSON para objeto
+        const data = JSON.parse(fileContents);
+        
+        return NextResponse.json({ data: data });
+
+    } catch (error) {
+        // O tratamento de erro é importante aqui
+        return NextResponse.json({ error: 'Failed to load public data' }, { status: 500 });
+    }
 }
